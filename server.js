12ef985b42b0ee137369b7fd46986b878e21f3de
renaclean <sk.sqm25@gmail.com>
2026-02-13 16:45:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const APK_DOWNLOAD_URL = 'https://github.com/renaclean/renacleandialyzer-server/
 // ⚙️ AUTHORIZED DEVICES DATABASE
 const authorizedDevices = new Set([
     '9d389cebf6a08dbd', //honor
-    '0e49ad2b70a6d384', //oneplus(signed apk)
+    '459e6d0b4391170f', //oneplus(signed apk)
 ]);
 
 // Logging
@@ -144,6 +144,7 @@ app.listen(PORT, '0.0.0.0', () => {
     log(`Authorized devices: ${authorizedDevices.size}`);
     log('==============================================');
 });
+
 
 
 
